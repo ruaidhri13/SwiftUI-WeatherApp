f8d2cf7f89456587c1896a82beecc7587b1850fb
Ruaidhri O'Neill <Ruaidhri2006@gmail.com>
2021-08-15 20:26:15 +0100

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Ruaidhrí O'Neill on 15/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
